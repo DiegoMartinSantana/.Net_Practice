83267721868449348318d45d28abe1be2087fff9
Diego Martín Santana <diego.santana@alumnos.frgp.utn.edu.ar>
2024-07-26 11:24:51 -0300

    Routing : Configuration , Get endPoint, Parameters  ,Default parameters, Constraints (custom and default). And wwwroot set.

---
+++ CustomConstraints/MonthsCustomConstraint.cs
@@ -0,0 +1,24 @@
+﻿
+using System.Text.RegularExpressions;
+
+namespace NetCoreProyectExample.CustomConstraints
+{
+    public class MonthsCustomConstraint : IRouteConstraint
+    {
+        public bool Match(HttpContext? httpContext, IRouter? route, string routeKey, RouteValueDictionary values, RouteDirection routeDirection)
+        {
+            if (!values.ContainsKey(routeKey))
+            {
+                return false; //not a match
+            }
+
+            Regex regex = new Regex("^(apr|jul|oct)$");
+            string? value = Convert.ToString(values[routeKey]);
+            if (regex.IsMatch(value))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,10 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Mvc.ModelBinding.Metadata;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Primitives;
+using NetCoreProyectExample.CustomConstraints;
 using NetCoreProyectExample.CustomMiddlewares;
+using System.Diagnostics.Metrics;
 
 namespace NetCoreProyectExample
 {
@@ -10,7 +13,11 @@ namespace NetCoreProyectExample
         public static void Main(string[] args)
         {
             //Create builder for web app
-            var builder = WebApplication.CreateBuilder(args);
+            var builder = WebApplication.CreateBuilder(new WebApplicationOptions()
+            {
+                WebRootPath = "myroot"
+            }
+                );
             //with that we can configure the web app or access to the services
             //builder.Configuration
             //
@@ -18,26 +25,230 @@ namespace NetCoreProyectExample
 
             builder.Services.AddTransient<MyCustomMiddleware>();
 
+            //add custom constraint 
+            builder.Services.AddRouting(opt =>
+            {
+                opt.ConstraintMap.Add("months", typeof(MonthsCustomConstraint));
+            });
+
             var app = builder.Build();//Build web app(config, default services etc)
                                       // .Build return a instance of object web app
                                       // app.MapGet("/", () => "Hi Future Developer!");
 
             //app run recibe un argumento  : httpcontext
-
-            #region Routing (5)
+            #region HomeWork (5) Routing 
             //enable routing
             app.UseRouting();
-            //create end points
-            app.UseEndpoints(endpoints =>
-            {
-                //add endpoints here
 
-            } );
+            Dictionary<int, string> Countries = new Dictionary<int, string>();
+            Countries.Add(1, "Usa");
+            Countries.Add(2, "Canada");
+            Countries.Add(3, "United Kingdom");
+            Countries.Add(4, "India");
+            Countries.Add(5, "Japan");
+
+            /*
+            Handler this dictionary with id request, all request and excepctions.
+            */
+            //Path : /Countries
+
+
+
+            //all
+            app.MapGet("Countries", async context =>
+            {
+                foreach (var country in Countries)
+                {
+                    await context.Response.WriteAsync(" " + country.Value+ "");
+                }
+
+            });
+
+            //create a class to define the constraints
+
+            //id
+            app.MapGet("Countries/{id:int}", async context =>
+            {
+                int id = Convert.ToInt32(context.Request.RouteValues["id"]);
+                if (id > 100 || id <0)
+                {
+                    context.Response.StatusCode = 400;
+                    await context.Response.WriteAsync("The value has not to be more at 100 at not less as 1");
+                    return;
+                }
+                if (!Countries.ContainsKey(id))
+                {
+                    context.Response.StatusCode = 404;
+                    await context.Response.WriteAsync("The value dont exists in the Bd.");
+                    return;
+                }
+
+                context.Response.StatusCode = 200;
+                await context.Response.WriteAsync("Country : " + Countries[id]);
+
+            });
 
             #endregion
 
 
-            #region HouseWork (4) Middlewares
+            #region Routing (5)
+
+            //create end points
+
+            //work with "myroot"
+            app.UseStaticFiles();
+
+            //work with another root 
+            app.UseStaticFiles(new StaticFileOptions()
+            {
+                FileProvider = new PhysicalFileProvider(Path.Combine(builder.Environment.ContentRootPath, "myotherwebroot"))
+            });
+
+
+            app.Map("/", async context =>
+            {
+                await context.Response.WriteAsync("hello");
+            });
+            //AFTER USE ROUTING. CAN SAVE THE ENDPOINT
+            Endpoint? endpoint = null;
+            app.Use(async (context, next) =>
+            {
+                endpoint = context.GetEndpoint();
+
+                await next();
+            });
+
+            //CAN ADD PARAMETERS TO THE PATH
+            //default parameter
+            app.MapGet("sellers/{country=argentina}", async (context) =>
+            {
+                string? country = Convert.ToString(context.Request.RouteValues["country"]);
+                await context.Response.WriteAsync("country = " + country);
+            });
+            //optional parameteres 
+            app.MapGet("buyers/{country?}", async (context) =>
+            {
+                if (context.Request.RouteValues.ContainsKey("country"))
+                {
+                    string? country = Convert.ToString(context.Request.RouteValues["country"]);
+                    await context.Response.WriteAsync("buyers = " + country);
+                }
+                else
+                {
+                    await context.Response.WriteAsync("parameter has not been passed");
+
+                }
+            });
+            //PARAMETER WITH CONSTRAINTS
+            app.MapGet("sellers33/{name:length(3,20)=pepe}", async context =>
+            {
+                string? name = Convert.ToString(context.Request.RouteValues["name"]);
+                await context.Response.WriteAsync($"name = {name}");
+
+            });
+            app.MapGet("sale/{year:int:min(2022)}/{month:regex(^(apr|jul|oct)$)}", async context =>
+            {
+                int valueyear = Convert.ToInt32(context.Request.RouteValues["year"]);
+                string? valuemonth = Convert.ToString(context.Request.RouteValues["month"]);
+                await context.Response.WriteAsync("the parameter its a date : " + valueyear + "month =" + valuemonth);
+
+            });
+            //but the best way is accept wrong values and response basement in that
+            app.MapGet("sale2/{year=2024}/{month=jul}", async context =>
+            {
+                int valueyear = Convert.ToInt32(context.Request.RouteValues["year"]);
+                string? valuemonth = Convert.ToString(context.Request.RouteValues["month"]);
+                if (valueyear > 2022 && valuemonth == "apr" || valuemonth == "jul" || valuemonth == "oct")
+                {
+                    await context.Response.WriteAsync("the parameter its a date : " + valueyear + "month =" + valuemonth);
+                }
+                else
+                {
+                    await context.Response.WriteAsync("invalid values");
+
+                }
+            });
+            app.MapGet("sale2/{year=2024}/{month:months}", async context =>
+{
+    int valueyear = Convert.ToInt32(context.Request.RouteValues["year"]);
+    string valuemonth = Convert.ToString(context.Request.RouteValues["month"]);
+    if (valueyear > 2022 && (valuemonth == "apr" || valuemonth == "jul" || valuemonth == "oct"))
+    {
+        await context.Response.WriteAsync("The parameter is a date: " + valueyear + " month = " + valuemonth);
+    }
+    else
+    {
+        await context.Response.WriteAsync("Invalid values");
+    }
+});
+
+
+            app.MapGet("sellers2/{name:int}", async context =>
+            {
+                int? value = Convert.ToInt32(context.Request.RouteValues["name"]);
+                await context.Response.WriteAsync("the parameter its a int  : " + value);
+
+            });
+            app.MapGet("sellersGuid/{id:guid}", async context =>
+            {
+                Guid? value = Guid.Parse(Convert.ToString(context.Request.RouteValues["id"]));
+                await context.Response.WriteAsync("guid value  : " + value);
+
+            });
+            app.MapGet("files/{filename}.{extension}", async (context) =>
+            {
+                string? file = Convert.ToString(context.Request.RouteValues["filename"]);
+                string? ext = Convert.ToString(context.Request.RouteValues["extension"]);
+                await context.Response.WriteAsync($"filename = {file} and extension is {ext}");
+            });
+
+
+            app.UseEndpoints(endpoints =>
+            {
+                //add endpoints here
+                endpoints.Map("mapExample", async (context) =>
+                {
+                    context.Response.StatusCode = 200;
+                    context.Response.Headers["Content-Type"] = "text/html";
+                    await context.Response.WriteAsync("in map example");
+                });
+                endpoints.Map("mapExample2", async (context) =>
+                {
+                    await context.Response.WriteAsync("in map example2");
+                    context.Response.StatusCode = 200;
+
+                });
+
+                app.MapGet("mapExample", async (context) =>
+                {
+                    context.Response.StatusCode = 200;
+
+                    await context.Response.WriteAsync("in getmapExample");
+                });
+            });
+
+            //important : 
+            app.MapGet("mapExample4", async (context) =>
+            {
+                await context.Response.WriteAsync("in getmapExample WITHOUT ENDPOINTS");
+
+            });
+
+
+            //if the url dont match execute that:
+            app.Run(async (context) =>
+            {
+                context.Response.Headers["Content-Type"] = "text/html";
+
+                string path = context.Request.Path;
+                await context.Response.WriteAsync("<h1>the path is : " + path + "</h1>");
+
+
+            });
+            #endregion
+
+
+            #region HomeWork (4) Middlewares
 
             app.UseMiniLoginMiddleware();
 
@@ -47,7 +258,7 @@ namespace NetCoreProyectExample
             /*
             recommended order to use middlewares
             Exception Handling
-    
+
     HTTPS Redirection
 
     Static Files
@@ -74,7 +285,7 @@ namespace NetCoreProyectExample
 
 
 
-         //   app.Map("/Hello", app.UseCustomMiddleware1); //
+            //   app.Map("/Hello", app.UseCustomMiddleware1); //
 
             //use  when allows conditions to execute middlewares
             app.UseWhen(
@@ -135,7 +346,7 @@ namespace NetCoreProyectExample
             */
 
 
-            #region HouseWork (3) Http
+            #region HomeWork (3) Http
             /*
             // received 3 values. two numbers (int)  and a operation simbole  (+,-,*,/ or %)
 
